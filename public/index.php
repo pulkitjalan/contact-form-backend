23572c0d23b74be20154b6536495a657fee2d9b9
pulkitjalan <pulkit1990@gmail.com>
2015-12-08 05:47:11 +0000

    added a required config

--- public/index.php
+++ public/index.php
@@ -14,48 +14,71 @@ $contact = new \PulkitJalan\ContactForm\Contact();
 header('Access-Control-Allow-Origin: *');
 
 // if form is not configured throw 500 error
-if (!$contact->isConfigured()) {
+if (! $contact->isConfigured()) {
     http_response_code(500);
     echo 'Contact form is not configured properly';
     exit;
 }
 
 // if request method was post then user is trying to send an email
-if ($input->isMethod('post')) {
-    $data = $input->all();
-    $files = array_get($data, 'files', []);
-
-    $data['files'] = array_map(function ($file) {
-        if (empty($file)) {
-            return;
-        }
-
-        return [
-            'path' => $file->getRealPath(),
-            'name' => urldecode($file->getClientOriginalName()),
-            'type' => $file->getMimeType(),
-        ];
-    }, ((is_array($files)) ? $files : [$files]));
-
-    // try sending
-    if ($contact->send($data)) {
-        echo 'Successful';
-
-        // Check if user has set a redirect
-        if ($success = $contact->getConfigParam('redirect.success', array_get($data, 'redirect.success'))) {
-            header('Location: '.$success);
-        }
-    } else {
-        echo 'Failed';
-
-         // Check if user has set a redirect
-        if ($failure = $contact->getConfigParam('redirect.failure', array_get($data, 'redirect.failure'))) {
-            header('Location: '.$failure);
-        }
-    }
-
+if (! $input->isMethod('post')) {
+    echo 'Contact form is up';
     exit;
 }
 
-echo 'Contact form is up';
-exit;
+$data = $input->all();
+
+$required = $contact->getConfigParam('required');
+foreach ($required as $validate) {
+    if (! in_array($validate, array_keys($data)) || is_null(array_get($data, $validate))) {
+        http_response_code(400);
+        echo 'Failed';
+
+        // Check if user has set a redirect
+        if ($failure = $contact->getConfigParam('redirect.failure', array_get($data, 'redirect.failure'))) {
+            header('Location: '.$failure);
+        }
+
+        exit;
+    }
+}
+
+$files = array_get($data, 'files', []);
+
+$data['files'] = array_map(function ($file) {
+    if (empty($file)) {
+        return;
+    }
+
+    return [
+        'path' => $file->getRealPath(),
+        'name' => urldecode($file->getClientOriginalName()),
+        'type' => $file->getMimeType(),
+    ];
+}, (array) $files);
+
+$success = false;
+try {
+    $success = $contact->send($data);
+} catch (Exception $e) {
+    error_log($e);
+    $success = false;
+}
+
+// try sending
+if ($success) {
+    echo 'Successful';
+
+    // Check if user has set a redirect
+    if ($success = $contact->getConfigParam('redirect.success', array_get($data, 'redirect.success'))) {
+        header('Location: '.$success);
+    }
+} else {
+    http_response_code(400);
+    echo 'Failed';
+
+    // Check if user has set a redirect
+    if ($failure = $contact->getConfigParam('redirect.failure', array_get($data, 'redirect.failure'))) {
+        header('Location: '.$failure);
+    }
+}
